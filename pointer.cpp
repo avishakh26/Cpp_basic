05d2d5e80fa89a35bee35020d36c7a4c0d39484b
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-01-19 19:21:44 +0600

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int x = 90;
+    int *p;
+
+    p = &x;
+                                                //ambersend &
+                                                //astrick p
+    cout << x <<endl;
+    cout << &x << endl;
+    cout << p ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
