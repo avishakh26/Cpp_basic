4be15ebc7e2d0627dd3499bdbe7825418db43aad
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-01-22 21:34:02 +0600

    Add files via upload

---
+++ while_loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+
+
+
+{
+
+    int i = 1;                      //initlization
+
+    while (i <= 10)         //condition
+    {
+
+        cout << i << endl;
+        i ++;           // increment
+
+    }
+
+
+    return 0;
+}
