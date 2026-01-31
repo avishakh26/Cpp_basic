bfe7eeab792195db066bab243c206d7267310457
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-01-31 23:54:34 +0600

    Add files via upload

---
+++ temp_convert.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std; 
+
+int main()
+
+{
+double cels , faren;
+
+cout << "Enter celsius = ";
+
+cin >> cels;
+
+
+
+faren = 1.8 * cels + 32;
+
+cout << "So the Farenhite is =" << faren;
+
+
+
+    getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
