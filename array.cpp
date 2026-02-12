28829d34b7f71ebd88d30e7267e64fb7a5e92034
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-02-12 21:00:58 +0600

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std ; 
+int main ()
+{
+
+    int marks[5] = {10 , 11, 12, 13 , 15};
+
+
+
+    for (int i = 0 ; i < 5 ; i ++){
+
+        cout << marks [i] << " " ;
+    }
+
+
+
+
+    return 0 ; 
+}
