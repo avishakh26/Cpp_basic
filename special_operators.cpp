eec188fec7fc07124337f37fe9b5befd9a57baf0
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-02-12 21:00:39 +0600

    Add files via upload

---
+++ special_operators.cpp
@@ -0,0 +1,67 @@
+/*#include<iostream>
+#include<conio.h>
+
+using namespace std ;
+
+int main() 
+
+
+{
+
+int a;
+float b;
+double c;
+char ch;
+char name [20];
+
+
+int z = sizeof(name);
+
+
+cout << z ;
+
+
+
+getch();
+}*/
+
+
+
+
+
+
+
+/*sizeof operator er kaj hoilo size bole deya. like kon size ache eda janano ekta data type koto size er eitah shey janabe. jmn amra jani
+integer ba float hoilo 4 bit kore kore , tai eta koy bit e ache eta show korbe */
+
+
+
+
+
+
+
+/*comma operation er khetre onkgula sub expression ke eksathe ekta operation e convert kora jay , jmn.....*/
+
+
+
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+
+{
+    int x , y , sum;;
+
+    sum = (x=20 , y=30 , sum = x+y);
+
+
+    cout << sum;
+
+
+
+
+    getch();
+}
