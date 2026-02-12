a3cf9dd3dbdb2e9565f2493bcd1e34bfec69075e
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-02-12 21:09:18 +0600

    Add files via upload

---
+++ logical_operator.cpp
@@ -0,0 +1,7 @@
+/*if er modde jodi onk gula condition thake r tader maje jodi logical and && thake taile jkhn shob gula condition
+true hobe tkhni kbl if block tah kaj korbe*/
+
+
+
+/*logical or '||'
+if er modde jekono ekta conditon true hoile oikhane if kaj korbe*/
